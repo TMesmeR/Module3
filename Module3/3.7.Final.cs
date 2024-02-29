fb9b47b9778b6b62964bd1e318e6d779a60ba040
TMesmeR <mesmerat@gmail.com>
2024-02-29 19:12:28 +0300

    Косметические изменения кода для 3.7.final и Program.cs

--- Module3/3.7.Final.cs
+++ Module3/3.7.Final.cs
@@ -11,11 +11,16 @@
                 var age = checked((byte)int.Parse(Console.ReadLine()));
                 Console.WriteLine();
                 Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+                
+                Console.WriteLine();
 
                 Console.Write("What is your favorite day of week? ");
                 var day = (DayOfWeek)int.Parse(Console.ReadLine());
+                Console.WriteLine();
                 Console.WriteLine("Your favorite day is {0}", day);
                 
+                Console.WriteLine();
+
                 Console.Write("Enter your birthday: ");
                 var birthDay = Console.ReadLine();
                 Console.WriteLine();
