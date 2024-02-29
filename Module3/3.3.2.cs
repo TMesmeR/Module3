287f3550243d9296992c8e6698dd3542df8dbe9b
TMesmeR <mesmerat@gmail.com>
2024-02-29 17:46:41 +0300

    Задание 3.3.2

---
+++ Module3/3.3.2.cs
@@ -0,0 +1,16 @@
+﻿internal static class Module_3_3_2
+{
+   static string MyName = "Jane";
+   static byte MyAge = 27;
+   static bool HaveIApet = true;
+   static double MyShoeSize = 37.5;
+
+   static internal void Start_3_3_2()
+    {
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
+    }
+}
+
